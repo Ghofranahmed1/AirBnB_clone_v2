6c90249439f66553108f662819733fa01bb62775
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-03-19 01:08:11 -0700

    task 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,17 @@
 -- prepares a MySQL server for the project
+-- create database
+-- create use named 'hbnb_dev' and grant all his/her privilages
+-- set a password to the user 'hbnb_dev_pwd'
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE USER 
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES 
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
